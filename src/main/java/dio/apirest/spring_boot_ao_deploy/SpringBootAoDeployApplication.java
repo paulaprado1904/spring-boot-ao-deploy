4ec5d5af625c04b868e8b3eb4ab0394017d8c840
paulaprado1904 <paula.prado@ufu.br>
2024-07-26 10:25:54 -0300

    Initial commit

---
+++ src/main/java/dio/apirest/spring_boot_ao_deploy/SpringBootAoDeployApplication.java
@@ -0,0 +1,13 @@
+package dio.apirest.spring_boot_ao_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAoDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAoDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/apirest/spring_boot_ao_deploy/SpringBootAoDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.apirest.spring_boot_ao_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAoDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
